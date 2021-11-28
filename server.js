6b97d662b0957e682a13d41ef0b97e7e75760764
joesteel <cculleton@expedia.com>
2021-11-28 19:58:48 +0000

    small fixes to infra buil files

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { hostname } = require('os');
 const http = require('http');
-const message = 'Hello Cloud... just no way this worked\n';
+const message = 'Hello Cloud... just no way this worked... did it?\n';
 const port = 8080;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
