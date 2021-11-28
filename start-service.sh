18f30ac567db86d9985741a531b243fbe7fac7bb
joesteel <cculleton@expedia.com>
2021-11-28 19:46:10 +0000

    fix to start/stop service

--- start-service.sh
+++ start-service.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
-[ -d "/home/ec2-user/app/release" ] && \
-cd /home/ec2-user/app/release && \
-npm stop
+cd /home/ec2-user/app/release
+npm run start 
